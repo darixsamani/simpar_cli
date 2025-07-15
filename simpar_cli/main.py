40e7bebf54ed64daa31e3ad9d1a10596de930b94
Darix SAMANI SIEWE <samanidarix@gmail.com>
2025-07-15 16:13:35 +0100

    uv and refactory

---
+++ simpar_cli/main.py
@@ -0,0 +1,15 @@
+import typer
+from simpar_cli.simpar import Simpar
+
+
+def main(image: str, imgr: str = "output.png"):
+    """
+    
+    """
+    simpar = Simpar(image, imgr)
+    simpar.simpar()
+    
+
+
+if __name__ == "__main__":
+    typer.run(main)
